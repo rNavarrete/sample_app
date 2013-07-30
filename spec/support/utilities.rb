5893a23a1372a94f07b6c926d37a889e5ae98f48
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-07-30 15:56:50 -0700

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,6 +7,16 @@ def full_title(page_title)
   end
 end
 
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
 RSpec::Matchers.define :have_error_message do |m|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: m)
