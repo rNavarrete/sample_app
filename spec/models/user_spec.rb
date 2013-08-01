6217b91e872d5bcf21545b74c572430bb433a264
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-07-31 20:15:16 -0700

    Added microposts in descending order, and destroying a user destroys their associated microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,6 +28,7 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
 
   it { should be_valid }
 
@@ -104,6 +105,33 @@ describe User do
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
+  end
+
+  describe "micropost associations" do 
+    
+    before { @user.save }
+    let!(:older_micropost) do 
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    
+    let!(:newer_micropost) do 
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago) 
+    end
+    
+    it "should destroy associated microposts" do 
+
+      microposts = @user.microposts.dup
+      @user.destroy
+      microposts.should_not be_empty
+      
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+
+    it "should have the right microposts in the right order" do 
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
   end
 
   describe "return value of authenticate method" do
