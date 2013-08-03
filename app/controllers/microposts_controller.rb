d152f9ed84a0da2b5a3d4f5179ba61533247abf0
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-08-03 15:53:38 -0700

    Added micrposts feed to user's home page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :correct_user,   only: :destroy
 
   
   def create
@@ -8,10 +9,24 @@ class MicropostsController < ApplicationController
   	  flash[:success] = "Micropost created!"
   	  redirect_to root_url
   	else
+      @feed_items = []
   	  render 'static_pages/home'
   	end    
   end
 	
   def destroy
+    @micropost.destroy
+    redirect_to root_url
   end
-end		
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by_id(params[:id])
+    redirect_to root_url if @micropost.nil?
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,8 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
+
 
   it { should be_valid }
 
@@ -117,6 +119,18 @@ describe User do
     let!(:newer_micropost) do 
       FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago) 
     end
+
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
     
     it "should destroy associated microposts" do 
 
@@ -148,5 +162,5 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
-  end
+  end  
 end
