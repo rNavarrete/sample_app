a190815b3bbea0676d086d6256c268bbd1d5d577
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-08-05 23:33:02 -0700

    Added a user and followers/following associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,5 @@
 # == Schema Information
-#
 # Table name: users
-#
 #  id         :integer          not null, primary key
 #  name       :string(255)
 #  email      :string(255)
@@ -30,6 +28,13 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
+
 
 
   it { should be_valid }
@@ -163,4 +168,27 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end  
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+  
+    describe "and unfollowing" do 
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its (:followed_users) { should_not include(other_user) }
+    end
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+     end
+  end
 end
