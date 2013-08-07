128b8b74cff9b1bbe2bbfe9e8025cd82da577bb1
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-08-06 20:01:14 -0700

    added-feed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,7 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com", 
+    @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
 
@@ -22,9 +22,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  it { should respond_to(:authenticate) }
-  it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
@@ -32,12 +31,29 @@ describe User do
   it { should respond_to(:followed_users) }
   it { should respond_to(:reverse_relationships) }
   it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
   it { should respond_to(:unfollow!) }
 
-
-
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "accessible attributes" do
+    it "should not allow access to admin" do
+      expect do
+        User.new(admin: true)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -55,28 +71,23 @@ describe User do
   end
 
   describe "when email format is invalid" do
-    invalid_addresses =  %w[user@foo,com user_at_foo.org example.user@foo.]
-    invalid_addresses.each do |invalid_address|
-      before { @user.email = invalid_address }
-      it { should_not be_valid }
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end
     end
   end
 
   describe "when email format is valid" do
-    valid_addresses = %w[user@foo.com A_USER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-    valid_addresses.each do |valid_address|
-      before { @user.email = valid_address }
-      it { should be_valid }
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end
     end
-  end
-
-  describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.save
-    end
-
-    it { should_not be_valid }
   end
 
   describe "when email address is already taken" do
@@ -87,6 +98,16 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
   end
 
   describe "when password is not present" do
@@ -104,53 +125,9 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "remember token" do
-    before { @user.save }
-    its(:remember_token) { should_not be_blank }
-  end
-
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
-  end
-
-  describe "micropost associations" do 
-    
-    before { @user.save }
-    let!(:older_micropost) do 
-      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
-    end
-    
-    let!(:newer_micropost) do 
-      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago) 
-    end
-
-
-    describe "status" do
-      let(:unfollowed_post) do
-        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
-      end
-
-
-      its(:feed) { should include(newer_micropost) }
-      its(:feed) { should include(older_micropost) }
-      its(:feed) { should_not include(unfollowed_post) }
-    end
-    
-    it "should destroy associated microposts" do 
-
-      microposts = @user.microposts.dup
-      @user.destroy
-      microposts.should_not be_empty
-      
-      microposts.each do |micropost|
-        Micropost.find_by_id(micropost.id).should be_nil
-      end
-    end
-
-    it "should have the right microposts in the right order" do 
-      @user.microposts.should == [newer_micropost, older_micropost]
-    end
   end
 
   describe "return value of authenticate method" do
@@ -167,7 +144,59 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
-  end  
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.dup
+      @user.destroy
+      microposts.should_not be_empty
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
+  end
 
   describe "following" do
     let(:other_user) { FactoryGirl.create(:user) }
@@ -178,17 +207,17 @@ describe User do
 
     it { should be_following(other_user) }
     its(:followed_users) { should include(other_user) }
-  
-    describe "and unfollowing" do 
-      before { @user.unfollow!(other_user) }
-
-      it { should_not be_following(other_user) }
-      its (:followed_users) { should_not include(other_user) }
-    end
 
     describe "followed user" do
       subject { other_user }
       its(:followers) { should include(@user) }
-     end
+    end
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,7 +14,6 @@ describe "User pages" do
     end
 
     it { should have_selector('title', text: 'All users') }
-    it { should have_selector('h1',    text: 'All users') }
 
     describe "pagination" do
 
@@ -92,56 +91,56 @@ describe "User pages" do
       it { should have_content(user.microposts.count) }
     end
 
-    #describe "follow/unfollow buttons" do
-      #let(:other_user) { FactoryGirl.create(:user) }
-      #before { sign_in user }
+    describe "follow/unfollow buttons" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { sign_in user }
 
-      #describe "following a user" do
-        #before { visit user_path(other_user) }
+      describe "following a user" do
+        before { visit user_path(other_user) }
 
-        #it "should increment the followed user count" do
-          #expect do
-            #click_button "Follow"
-          #end.to change(user.followed_users, :count).by(1)
-        #end
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
 
-        #it "should increment the other user's followers count" do
-          #expect do
-            #click_button "Follow"
-          #end.to change(other_user.followers, :count).by(1)
-        #end
+        it "should increment the other user's followers count" do
+          expect do
+            click_button "Follow"
+          end.to change(other_user.followers, :count).by(1)
+        end
 
-        #describe "toggling the button" do
-          #before { click_button "Follow" }
-          #it { should have_selector('input', value: 'Unfollow') }
-        #end
-      #end
+        describe "toggling the button" do
+          before { click_button "Follow" }
+          it { should have_selector('input', value: 'Unfollow') }
+        end
+      end
 
-      #describe "unfollowing a user" do
-        #before do
-          #user.follow!(other_user)
-          #visit user_path(other_user)
-        #end
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
 
-        #it "should decrement the followed user count" do
-          #expect do
-            #click_button "Unfollow"
-          #end.to change(user.followed_users, :count).by(-1)
-        #end
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(user.followed_users, :count).by(-1)
+        end
 
-        #it "should decrement the other user's followers count" do
-          #expect do
-            #click_button "Unfollow"
-          #end.to change(other_user.followers, :count).by(-1)
-        #end
+        it "should decrement the other user's followers count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
 
-        #describe "toggling the button" do
-          #before { click_button "Unfollow" }
-          #it { should have_selector('input', value: 'Follow') }
-        #end
-      #end
+        describe "toggling the button" do
+          before { click_button "Unfollow" }
+          it { should have_selector('input', value: 'Follow') }
+        end
+      end
     end
-  #end
+  end
 
   describe "signup page" do
     before { visit signup_path }
@@ -198,8 +197,7 @@ describe "User pages" do
     before do
       sign_in user
       visit edit_user_path(user)
-    end  
-
+    end
 
     describe "page" do
       it { should have_selector('h1',    text: "Update your profile") }
@@ -209,17 +207,17 @@ describe "User pages" do
 
     describe "with invalid information" do
       before { click_button "Save changes" }
+
       it { should have_content('error') }
     end
 
     describe "with valid information" do
       let(:new_name)  { "New Name" }
       let(:new_email) { "new@example.com" }
-      
       before do
-        fill_in "Name", with: new_name
-        fill_in "Email", with: new_email
-        fill_in "Password", with: user.password
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
         fill_in "Confirm Password", with: user.password
         click_button "Save changes"
       end
@@ -232,31 +230,31 @@ describe "User pages" do
     end
   end
 
-  #describe "following/followers" do
-    #let(:user) { FactoryGirl.create(:user) }
-    #let(:other_user) { FactoryGirl.create(:user) }
-    #before { user.follow!(other_user) }
+  describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
 
-    #describe "followed users" do
-      #before do
-        #sign_in user
-        #visit following_user_path(user)
-      #end
+    describe "followed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
 
-      #it { should have_selector('title', text: full_title('Following')) }
-      #it { should have_selector('h3', text: 'Following') }
-      #it { should have_link(other_user.name, href: user_path(other_user)) }
-    #end
+      it { should have_selector('title', text: full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
 
-    #describe "followers" do
-      #before do
-        #sign_in other_user
-        #visit followers_user_path(other_user)
-      #end
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
 
-      #it { should have_selector('title', text: full_title('Followers')) }
-      #it { should have_selector('h3', text: 'Followers') }
-      #it { should have_link(user.name, href: user_path(user)) }
-    #end
-  #end
+      it { should have_selector('title', text: full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
+    end
+  end
 end
